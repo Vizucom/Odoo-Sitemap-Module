b09a9afa42b1a520be38af91197f7268ce4ffab1
Robert Hofer <robert.hofer@technikum-wien.at>
2015-05-07 16:38:28 +0200

    settings backend view

--- __openerp__.py
+++ __openerp__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        # 'views/backend/settings.xml',
+        'views/backend/settings.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
